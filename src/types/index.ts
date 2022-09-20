43d7e106b9abae9adb1953e527fa877e81fd99b2
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-20 16:26:54 +0800

    create payload type for ProfileForm

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,19 @@
 export interface IRoute {
-    path: string,
-    component: any
+  path: string;
+  component: any;
+}
+
+export interface IWorkExperience {
+    title: string,
+    company: string,
+    image: File,
+    jobDescription: string,
+    startDate: string,
+    endDate?: string, // if current position, user will not have end date
+    isCurrentPosition: boolean
+}
+export interface IProfileFormPayload {
+  name: string;
+  age: number;
+  workExperience: IWorkExperience[];
 }
