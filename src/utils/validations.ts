f730a35aa8a4ac6e209ef3f9b10fe1121d6c6be2
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-26 19:26:39 +0800

    update validation schema

---
+++ src/utils/validations.ts
@@ -0,0 +1,57 @@
+import * as yup from "yup";
+
+export const validationSchema = yup.object({
+  profileImage: yup.mixed().required("We're gonna need your picture, buddy."),
+  name: yup
+    .string()
+    .required("A name is required!")
+    .matches(
+      /^[a-zA-Z]+$/g,
+      "A name can't contain numbers/special characters. We're not progressive like that."
+    ),
+  age: yup
+    .number()
+    .required("Your age is required.")
+    .positive("You aren't born yet.")
+    .integer("Please enter a number!")
+    .test("age", "You gotta be above 18 to apply mate.", function (value: any) {
+      const isOfLegalAge: boolean = value > 18;
+      return isOfLegalAge;
+    })
+    .typeError("Age has to be a number!"),
+  workExperiences: yup
+    .array()
+    .of(
+      yup.object({
+        title: yup
+          .string()
+          .required("Gotta know your job title.")
+          .matches(
+            /^[a-zA-Z]+$/g,
+            "Your job title can't contain special characters. That job can't really exist."
+          ),
+        company: yup.string().required("We need to know where you work at."),
+        companyLogoImage: yup
+          .mixed()
+          .required(
+            "We're gonna need your company picture too, for some reason"
+          ),
+        jobDescription: yup.string().required("Describe your job, baby."),
+        startDate: yup.date().required("Please enter a valid date.").nullable(),
+        endDate: yup // TODO: Need to check how required interacts with UI if TextField for end date is not present
+          .date()
+          .typeError("You didn't input a date?")
+          .required()
+          .when("startDate", (startDate) =>
+            startDate
+              ? yup
+                  .date()
+                  .min(startDate, "End date can't be earlier than start date!")
+                  .typeError("End date is required")
+              : yup.date()
+          ),
+        isCurrentPosition: yup.boolean(),
+      })
+    )
+    .required(),
+});
