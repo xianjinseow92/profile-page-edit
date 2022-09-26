f4c715f2c31acbf6108910e685f879ac204ca3cf
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-26 23:50:29 +0800

    rename ProfilePictureInput to PictureInput for more accurate description

---
+++ src/components/ui-components/PictureInput.tsx
@@ -0,0 +1,59 @@
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/system/Box";
+import FormHelperText from "@mui/material/FormHelperText";
+import PreviewImage from "./PreviewImage";
+
+const PictureInput = (props: any) => {
+  const {
+    id,
+    name,
+    label,
+    onChange,
+    onBlur,
+    error,
+    helperText,
+    touched,
+    value,
+    sx,
+    placeholderImage,
+  } = props;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <IconButton
+        color="primary"
+        aria-label="upload picture"
+        component="label"
+        disableRipple
+      >
+        <input
+          hidden
+          accept="image/*"
+          type="file"
+          id={id}
+          name={name}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        <PreviewImage
+          file={value}
+          label={label}
+          sx={{ ...sx, border: error ? "solid 1px red" : "" }}
+          placeholderImage={placeholderImage}
+        />
+      </IconButton>
+      {label && !error && !touched && <FormHelperText>{label}</FormHelperText>}
+      {error && helperText && (
+        <FormHelperText error>{helperText}</FormHelperText>
+      )}
+    </Box>
+  );
+};
+
+export default PictureInput;
