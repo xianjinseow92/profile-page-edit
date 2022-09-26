0038ed4ea6ddb99d469b1608726b5772ac9a62b2
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-26 16:20:06 +0800

    create card component to segregate sections

---
+++ src/components/layout/CardWithLabel.tsx
@@ -0,0 +1,20 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+const CardWithLabel = (props: any) => {
+  const { title, children } = props;
+
+  return (
+    <Card sx={{ display: "flex", width: "100%" }}>
+      <CardContent sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <Typography variant="h5" sx={{ fontWeight: "bold" }} gutterBottom>
+          {title}
+        </Typography>
+        {children}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CardWithLabel;
