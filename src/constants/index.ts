311972b5d862fe05ae2977ab32e5d05c7a891924
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-29 17:22:26 +0800

    update constatns

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,6 @@
 export const DATE_FORMAT = "DD/MM/YYYY";
+
+// Firebase collections
+export const FIREBASE_USER_PROFILE_COLLECTION = "user-profiles"
+
+export const FIREBASE_STORAGE_IMAGE_PATH = "images/";
