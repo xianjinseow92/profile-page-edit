8d7f6ad0d22b233807c4acf8e4753db77fb20411
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-30 14:26:38 +0800

    Implement Alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "styles/index";
+import Alert from "react-s-alert";
+import "react-s-alert/dist/s-alert-default.css";
+import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
 
 // Routes
 import AppRoutes from "routes/AppRoutes";
@@ -9,6 +12,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppRoutes />
+      <Alert stack={{ limit: 1 }} html position="top" effect="stackslide" />
     </ThemeProvider>
   );
 }
