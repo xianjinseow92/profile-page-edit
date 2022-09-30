aa39356223818e331cccc4ffbee379431afca1b2
Xian Jin Seow <xianjin.seow@circles.asia>
2022-09-30 14:27:01 +0800

    add styling for scrollOptions

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,9 +1,22 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        mode: "light"
-    }
+  palette: {
+    mode: "light",
+  },
 });
+
+export const scrollToOptions = (field: string) => {
+  let offset = 0;
+  if (field.includes("workExperience")) {
+    offset = -50;
+  }
+  return {
+    duration: 500,
+    delay: 100,
+    smooth: true,
+    offset: offset,
+  };
+};
 
 export default responsiveFontSizes(theme);
